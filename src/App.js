5594df273eb4f1857ca3da3796f0ea90b2ebea98
jeffxson <65169599+jeffxson@users.noreply.github.com>
2021-09-01 16:24:58 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Homescreen from './profolio/Homescreen'
 import AboutScreen from './profolio/AboutScreen'
 import Resume from './profolio/Resume'
-import{Container, Row, Col} from 'react-bootstrap'
+import{Container} from 'react-bootstrap'
 import Header from './profolio/container/Header'
 import Fotter from './profolio/container/Fotter'
 
